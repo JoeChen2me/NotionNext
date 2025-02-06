405d3becc45ced65600f07206b767eccf2f7cda1
JoeChen <1831427532@qq.com>
2025-02-06 20:08:49 +0800

    更新英雄区分类链接，将“必看精选”的URL修改为“推荐”

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -28,7 +28,7 @@ const CONFIG = {
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
+  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/推荐' },
   HEO_HERO_CATEGORY_2: { title: '技术分享', url: '/category/技术分享' },
   HEO_HERO_CATEGORY_3: { title: '学习笔记', url: '/category/学习笔记' },
 
